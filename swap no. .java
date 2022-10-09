4d9a83ed3aa995df07fde6ac34b346e8fdb82809
Deepali gupta <95581953+Deepaligup-ta@users.noreply.github.com>
2022-10-09 11:38:27 +0530

    swap no. .java

---
+++ swap no. .java
@@ -0,0 +1,32 @@
+
+import java.util.*;
+ 
+class main {
+ 
+  
+    static void swapValuesWithoutUsingThirdVariable(int m,
+                                                    int n)
+    {
+m = m - n;
+ 
+       
+        n = m + n;
+ 
+      
+        m = n - m;
+ 
+      
+        System.out.println("Value of m is " + m
+                           + " and Value of n is " + n);
+    }
+ 
+   ]
+    public static void main(String[] args)
+    {
+      
+        int m = 9, n = 5;
+ 
+      
+        swapValuesWithoutUsingThirdVariable(m, n);
+    }
+}
