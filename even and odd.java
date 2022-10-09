3f3b5e21039518efedc509d6d2d14875f3f43fb2
Deepali gupta <95581953+Deepaligup-ta@users.noreply.github.com>
2022-10-09 11:52:20 +0530

    even and odd.java

---
+++ even and odd.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.Scanner;
+ 
+
+class main {
+ 
+    
+    public static void main(String[] args)
+    {
+   
+        int num ;
+        Scanner s=new Scanner(System.in)
+
+ 
+        
+        if (num % 2 == 0) {
+ 
+            
+            System.out.println("Entered Number is Even");
+        }
+ 
+        else {
+ 
+           
+            System.out.println("Entered Number is Odd");
+        }
+    }
+}
